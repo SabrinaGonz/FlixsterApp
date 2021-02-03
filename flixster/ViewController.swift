bdae3185848186edf7fc2610f2003a78875832d5
sabrina-git <sabrina-git@users.noreply.github.com>
2021-02-02 19:18:48 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Sabrina Gonzalez on 2/2/21.
+//  Copyright © 2021 sabbiegonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
