b32bce8e71d161944d8b0561e3535da2f3951f7b
sabrina-git <sabrina-git@users.noreply.github.com>
2021-02-09 00:06:55 -0800

    added background pictures

--- MovieDetailsViewController.swift
+++ MovieDetailsViewController.swift
@@ -30,7 +30,10 @@ class MovieDetailsViewController: UIViewController {
         
         
         titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
         synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
@@ -38,7 +41,10 @@ class MovieDetailsViewController: UIViewController {
         
         posterView.af_setImage(withURL:posterUrl)
         
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)!
         
+        backdropView.af_setImage(withURL:backdropUrl)
         
     }
     
