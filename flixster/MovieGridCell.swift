fe6d0f3f58e79d65ec623d5336158a241cadfa08
sabrina-git <sabrina-git@users.noreply.github.com>
2021-02-09 16:14:53 -0800

    Collection View Setup

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Sabrina Gonzalez on 2/9/21.
+//  Copyright © 2021 sabbiegonzalez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
